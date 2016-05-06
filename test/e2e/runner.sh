7d1fae66fa59a0b6adc3f3e00b27d1e7a5734ea8
Allen Yao <alleyao@gmail.com>
2016-05-06 14:17:46 +0000

    Use xvfb-run instead of Xvfb

--- test/e2e/runner.sh
+++ test/e2e/runner.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-Xvfb :99 -screen 0 1024x768x16 >/dev/null &
-export DISPLAY=:99
-node ./test/e2e/runner.js
-killall Xvfb
+xvfb-run --server-args="-screen 0 1024x768x16" node ./test/e2e/runner.js
